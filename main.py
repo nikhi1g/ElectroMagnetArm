fa9cc562c3ac17ef6af868cd9549990d6aaf7dee
Nikhil Gupta <nikhilgupta@dpengineering.org>
2021-10-06 11:27:39 -0700

    fully working joystick

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ from pidev.Joystick import Joystick # buttons are actually buttons -1
 from pidev.kivy import DPEAButton
 from pidev.kivy import PauseScreen
 from time import sleep
-import RPi.GPIO as GPIO 
+import RPi.GPIO as GPIO
 from pidev.stepper import stepper
 from pidev.Cyprus_Commands import Cyprus_Commands_RPi as cyprus
 
@@ -92,7 +92,9 @@ arm = stepper(port = 0, speed = 10)
 # //                       MAIN FUNCTIONS                       //
 # //             SHOULD INTERACT DIRECTLY WITH HARDWARE         //
 # ////////////////////////////////////////////////////////////////
-	
+
+
+
 class MainScreen(Screen):
     cyprus.setup_servo(2)
     version = cyprus.read_firmware_version()
@@ -140,32 +142,80 @@ class MainScreen(Screen):
     def joy_update(self):
         while True:
             #self.joy_y_val = self.joystick.get_axis
+            #simple rotation
+            if self.joystick.get_button_state(3) == 1: #Actually button labeled 4
+                self.s0.setMaxSpeed(100)
+                self.s0.relative_move(-2)
 
-            if self.joystick.get_axis('x') == 0:
+            if self.joystick.get_button_state(4) == 1: #Actually button labeled 5
+                self.s0.setMaxSpeed(100)
+                self.s0.relative_move(2)
+            #stop
+            if -0.2 < self.joystick.get_axis('x') < 0.2: # Stop At 0, contingent on the 0.2 bounds
                 self.softstop()
 
-            if self.joystick.get_button_state(0) == 1:
+            #auto, probably will break;
+            if self.joystick.get_button_state(1) == 1 or self.joystick.get_button_state(2) == 1: #if either button labeled 3 or 2 is pressed
+                cyprus.set_pwm_values(1, period_value=100000, compare_value=0, compare_mode=cyprus.LESS_THAN_OR_EQUAL)#set arm to up pos
+                if self.magnetControl.text == 'Magnet Off': #magnet set to on
+                    self.toggleMagnet()
+                self.s0.setMaxSpeed(50)
+                self.s0.relative_move(2)
+                self.isbusy()
+                if (cyprus.read_gpio() & 0b0010) == 0: #port 6 tall
+                    self.s0.relative_move(-0.8)
+                    self.isbusy()
+                    sleep(1)
+                    self.armDown()
+                    self.armUp()
+                    self.s0.relative_move(0.35)
+                    self.armDown()
+                    self.toggleMagnet()
+                    self.armUp()
+                    self.home()
+                    self.toggleMagnet()
+
+
+
+                elif (cyprus.read_gpio() & 0b0001) == 0: #port 7 short
+                    self.s0.relative_move(-0.45)
+                    self.isbusy()
+                    sleep(1)
+                    self.armDown()
+                    self.armUp()
+                    self.s0.relative_move(-0.35)
+                    self.armDown()
+                    self.toggleMagnet()
+                    self.armUp()
+                    self.home()
+                    self.toggleMagnet()
+
+            if self.joystick.get_button_state(0) == 1: #Trigger ButtonMagnet
                 self.toggleMagnet()
                 sleep(0.3)
 
-            if self.joystick.get_axis('x') > 0:
+            #leftright swivel
+            if self.joystick.get_axis('x') > 0.2:
                 self.softstop()
                 print(self.joystick.get_axis('x'))
-                for i in range(0,21):
-                    self.s0.start_relative_move(0.2)
-            if self.joystick.get_axis('x') < 0:
+                for i in range(0,24):
+                    self.s0.start_relative_move(0.3)
+            if self.joystick.get_axis('x') < -0.2:
                 self.softstop()
                 print(self.joystick.get_axis('x'))
-                for i in range(0,21):
-                    self.s0.start_relative_move(-0.2)
+                for i in range(0,24):
+                    self.s0.start_relative_move(-0.3)
 
+            #piston updown
 
             if self.joystick.get_axis('y') > 0.5:
+                print("MovingUp")
                 cyprus.set_pwm_values(1, period_value=100000, compare_value=0, compare_mode=cyprus.LESS_THAN_OR_EQUAL)
             if self.joystick.get_axis('y') < -0.5:
+                print("MovingDown")
                 cyprus.set_pwm_values(1, period_value=100000, compare_value=100000, compare_mode=cyprus.LESS_THAN_OR_EQUAL)
 
-    def toggleMagnet(self):
+    def toggleMagnet(self): #turns magnet on and off
         if self.magnetControl.text == "Magnet Off":
             cyprus.set_servo_position(2, 0)
             self.magnetControl.text = "Magnet On"
@@ -175,26 +225,48 @@ class MainScreen(Screen):
             self.magnetControl.color = 1, 0, 0, 1
             self.magnetControl.text = "Magnet Off"
 
-    def softstop(self):
+    def softstop(self): #s0.softstop shortcut
         self.s0.softStop()
+
+    def armDown(self):
+        cyprus.set_pwm_values(1, period_value=100000, compare_value=100000, compare_mode=cyprus.LESS_THAN_OR_EQUAL)
+        sleep(3)
+    def armUp(self):
+        cyprus.set_pwm_values(1, period_value=100000, compare_value=0, compare_mode=cyprus.LESS_THAN_OR_EQUAL)
+        sleep(3)
+
+    def tallStand(self):
+        if (cyprus.read_gpio() & 0b0001) == 0: #port 7 which is short
+            return True
+
+    def shortStand(self):
+        if (cyprus.read_gpio() & 0b0010) == 0:#port 6 which is tall
+            return True
+
+    def isbusy(self):
+        while self.s0.isBusy():
+            sleep(0.3)
+
+
 
     def auto(self):
         print("Run the arm automatically here")
 
     def setArmPosition(self, position):
-        print("Blue means off, red means off")
+        print("Blue means on, red means off")
 
-
+    def home(self):
+        self.s0.relative_move(2)
 
     def homeArm(self):
         arm.home(self.homeDirection)
-        
+
     def isBallOnTallTower(self):
         print("Determine if ball is on the top tower")
 
     def isBallOnShortTower(self):
         print("Determine if ball is on the bottom tower")
-        
+
     def initialize(self):
         print("Home arm and turn off magnet")
 
@@ -205,7 +277,7 @@ class MainScreen(Screen):
 
     def quit(self):
         MyApp().stop()
-    
+
 sm.add_widget(MainScreen(name = 'main'))
 
 
